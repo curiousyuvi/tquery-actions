cb2eb5ce54dc1d3b34607ade17275ea8ac4ce3d7
Yuvraj Singh <ysgaur9919@gmail.com>
2023-03-02 21:14:09 +0530

    added export for QueryClientProvider, QueryClient and useQueryClient

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export { useGetCharacters } from './hooks/useGetCharacters';
 export { useCreateCharacter } from './hooks/useCreateCharacter';
 export { useGetCharacter } from './hooks/useGetCharacter';
 export { useUpdateCharacter } from './hooks/useUpdateCharacter';
+export {
+  QueryClientProvider as TQueryClientProvider,
+  QueryClient as TQueryClient,
+  useQueryClient as useTQueryClient,
+} from '@tanstack/react-query';
