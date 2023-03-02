730d9267d4137d120ffa59b9ed6880ca769f6e7a
Yuvraj Singh <ysgaur9919@gmail.com>
2023-03-02 21:34:48 +0530

    some fixes in useGetCharacter

--- src/hooks/useGetCharacter/index.ts
+++ src/hooks/useGetCharacter/index.ts
@@ -2,17 +2,20 @@ import * as request from 'superagent';
 import { Character } from '../../interfaces/Character';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
-type GetCharacterAPI = () => Promise<Character>;
+type GetCharacterAPI = (id: number) => Promise<Character>;
 type UseGetCharacter = (id: number) => UseQueryResult<Character, any>;
 
 const API_ENDPOINT = 'https://characters-jsonserver.up.railway.app/characters';
 
-const getCharacterAPI: GetCharacterAPI = async () => {
-  const res = await request.get(API_ENDPOINT);
+const getCharacterAPI: GetCharacterAPI = async (id) => {
+  const res = await request.get(API_ENDPOINT + `/${id}`);
   return res.body;
 };
 
 export const useGetCharacter: UseGetCharacter = (id) => {
-  const query = useQuery({ queryKey: ['characters', id], queryFn: getCharacterAPI });
+  const query = useQuery({
+    queryKey: ['characters', id],
+    queryFn: () => getCharacterAPI(id),
+  });
   return query;
 };
